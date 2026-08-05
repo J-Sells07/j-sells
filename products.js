a38e72f7daad32bfb49ef61d7fcf1c5606818ccc
J-Sells07 <Jseller1to1@yahoo.com>
2026-08-04 21:48:03 -0400

    Add new product entries to products.js

--- products.js
+++ products.js
@@ -12,6 +12,62 @@ const products = [
 
         category: "Electronics"
 
+    },
+
+    {
+        id: 2,
+        name: "Creed Aventus Cologne",
+        price: 50,
+        image: "images/creed-aventus.jpg",
+        category: "Fragrance"
+    },
+
+    {
+        id: 3,
+        name: "AirPods Max",
+        price: 160,
+        image: "images/airpods-max.jpg",
+        category: "Electronics"
+    },
+
+    {
+        id: 4,
+        name: "Sp5der Hoodie",
+        price: 80,
+        image: "images/sp5der-hoodie.jpg",
+        category: "Streetwear"
+    },
+
+    {
+        id: 5,
+        name: "Chrome Hearts Hoodie",
+        price: 70,
+        image: "images/chrome-hearts-hoodie.jpg",
+        category: "Streetwear"
+    },
+
+    {
+        id: 6,
+        name: "Denim Shorts",
+        price: 50,
+        image: "images/denim-shorts.jpg",
+        category: "Clothing"
+    },
+
+    {
+        id: 7,
+        name: "Yeezy Slides",
+        price: 50,
+        image: "images/yeezy-slides.jpg",
+        category: "Footwear"
+    },
+
+    {
+        id: 8,
+        name: "Jordan 4's",
+        price: 80,
+        image: "images/jordan4-black-cat.jpg",
+        category: "Sneakers"
     }
 
 ];
